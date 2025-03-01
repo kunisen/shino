22fba611f459d83c7ba9a583f5e6277abf48918e
Kuni Sen <kuniyasu.sen@gmail.com>
2025-03-01 21:22:38 +0900

    average prediction is done

--- 02_Python/01_APlusChecker/GradeChecker.py
+++ 02_Python/01_APlusChecker/GradeChecker.py
@@ -2,6 +2,7 @@ import math
 
 
 print ("Shino's Grade Calculator!")
+print ("===============================================")
 
 ## Environmental Variables & Data Sets Definition
 
@@ -19,3 +20,48 @@ GradeCurrent = (AverageSummative) * 0.6 + (AverageFormative) *  0.4
 RoundedUpGradeCurrent = round(GradeCurrent, 4) * 100
 
 print ("Current Grade is ", RoundedUpGradeCurrent)
+
+print ("===============================================")
+
+## Calculate Prediction Average
+
+### First, let's not use the stdin (keyboard)
+# NewAssignmentType = "Summative"
+# NewPointsEarned = 10
+# NewPointsPossible = 12
+
+### Let's use keyboard input
+NewAssignmentType = input("Input: New assignment Type - summative or formative: ")
+NewPointsEarned = int(input("Input: New Points Earned / Points Earned Prediction: "))
+NewPointsPossible = int(input("Input: New Points Possible / Points Possible Prediction: "))
+
+print ("New assignment type is ", NewAssignmentType)
+
+print ("New Points Earned is ", NewPointsEarned)
+print ("New Points Possible is ", NewPointsPossible)
+
+### Calculate Prediction Average Based on the input
+if NewAssignmentType.lower() == "summative":
+    PointsEarnedSummativePrediction = NewPointsEarned
+    PointsPossibleSummativePrediction = NewPointsPossible
+
+    AverageSummativePrediction = (SumOfPointsEarnedSummative + PointsEarnedSummativePrediction) / (SumOfPointsPossibleSummative + PointsPossibleSummativePrediction)
+    RoundedUpAverageSummativePrediction = round(AverageSummativePrediction, 4) * 100
+    print ("Average prediction is", RoundedUpAverageSummativePrediction)
+
+elif NewAssignmentType.lower() == "formative":
+    PointsEarnedFormativePrediction = NewPointsEarned
+    PointsPossibleFormativePrediction = NewPointsPossible
+
+    AverageForamtivePrediction = (SumOfPointsEarnedFormative + PointsEarnedFormativePrediction) / (SumOfPointsPossibleFormative + PointsPossibleFormativePrediction)
+    RoundedUpAverageFormativePrediction = round(AverageForamtivePrediction, 4) * 100
+    print ("Average prediction is", RoundedUpAverageFormativePrediction)
+
+else:
+    print ("wrong assignment type! needs to be either summative or formative")
+
+
+
+
+
+
